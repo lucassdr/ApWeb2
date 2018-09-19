76636476b402c6d30988f7a26588a304787ecc11
Lucas Sodré <lucassodre1996@gmail.com>
2018-09-19 00:51:05 -0300

    Alteração na classe de dominio Funcionario

--- Sicre/src/modelo/dominio/Funcionario.java
+++ Sicre/src/modelo/dominio/Funcionario.java
@@ -17,10 +17,16 @@ public class Funcionario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FUNCIONARIO_PK")
-	private Integer id;
+	private Integer matricula;
 
 	@Column(name = "NOME", nullable = false, length = 100)
 	private String nome;
+
+	@Column(name = "SOBRENOME", nullable = false)
+	private String sobrenome;
+
+	@Column(name = "TIPO_SANGUINEIO", nullable = false)
+	private String tipoSanguineo;
 
 	@Column(name = "DATA_NASCIMENTO", length = 10, nullable = false)
 	private String dataNascimento;
@@ -28,8 +34,17 @@ public class Funcionario {
 	@Column(name = "SEXO", length = 10)
 	private String sexo;
 
-	@Column(name = "MATRICULA", length = 20, unique = true, nullable = false)
-	private String matricula;
+	@Column(name = "CPF", length = 14, nullable = false, unique = true)
+	private String cpf;
+
+	@Column(name = "RG", length = 50, unique = true)
+	private String rg;
+
+	@Column(name = "SENHA", length = 50, nullable = false)
+	private String senha;
+
+	// @Column(name = "MATRICULA", length = 20, unique = true, nullable = false,)
+	// private String matricula;
 
 	@ManyToOne
 	@JoinColumn(name = "id_empresa_fk")
@@ -39,21 +54,27 @@ public class Funcionario {
 		super();
 	}
 
-	public Funcionario(Integer id, String nome, String dataNascimento, String sexo, String matricula) {
+	public Funcionario(Integer matricula, String nome, String sobrenome, String tipoSanguineo, String dataNascimento,
+			String sexo, String cpf, String rg, String senha, Empresa empresa) {
 		super();
-		this.id = id;
+		this.matricula = matricula;
 		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.tipoSanguineo = tipoSanguineo;
 		this.dataNascimento = dataNascimento;
 		this.sexo = sexo;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.senha = senha;
+		this.empresa = empresa;
+	}
+
+	public Integer getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Integer matricula) {
 		this.matricula = matricula;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
 	}
 
 	public String getNome() {
@@ -62,6 +83,22 @@ public class Funcionario {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getTipoSanguineo() {
+		return tipoSanguineo;
+	}
+
+	public void setTipoSanguineo(String tipoSanguineo) {
+		this.tipoSanguineo = tipoSanguineo;
 	}
 
 	public String getDataNascimento() {
@@ -80,12 +117,28 @@ public class Funcionario {
 		this.sexo = sexo;
 	}
 
-	public String getMatricula() {
-		return matricula;
+	public String getCpf() {
+		return cpf;
 	}
 
-	public void setMatricula(String matricula) {
-		this.matricula = matricula;
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public Empresa getEmpresa() {
@@ -100,12 +153,16 @@ public class Funcionario {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
 		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
 		result = prime * result + ((empresa == null) ? 0 : empresa.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
 		result = prime * result + ((sexo == null) ? 0 : sexo.hashCode());
+		result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+		result = prime * result + ((tipoSanguineo == null) ? 0 : tipoSanguineo.hashCode());
 		return result;
 	}
 
@@ -118,6 +175,11 @@ public class Funcionario {
 		if (getClass() != obj.getClass())
 			return false;
 		Funcionario other = (Funcionario) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
 		if (dataNascimento == null) {
 			if (other.dataNascimento != null)
 				return false;
@@ -127,11 +189,6 @@ public class Funcionario {
 			if (other.empresa != null)
 				return false;
 		} else if (!empresa.equals(other.empresa))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
 			return false;
 		if (matricula == null) {
 			if (other.matricula != null)
@@ -143,10 +200,30 @@ public class Funcionario {
 				return false;
 		} else if (!nome.equals(other.nome))
 			return false;
+		if (rg == null) {
+			if (other.rg != null)
+				return false;
+		} else if (!rg.equals(other.rg))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
 		if (sexo == null) {
 			if (other.sexo != null)
 				return false;
 		} else if (!sexo.equals(other.sexo))
+			return false;
+		if (sobrenome == null) {
+			if (other.sobrenome != null)
+				return false;
+		} else if (!sobrenome.equals(other.sobrenome))
+			return false;
+		if (tipoSanguineo == null) {
+			if (other.tipoSanguineo != null)
+				return false;
+		} else if (!tipoSanguineo.equals(other.tipoSanguineo))
 			return false;
 		return true;
 	}
