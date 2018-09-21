ba7131dfd16313637f9ba93031b19152d43cf309
Lucas Sodré <lucassodre1996@gmail.com>
2018-09-21 15:08:53 -0300

    Criação da classe de caminhos URL
    Nesta classe serão armazenadas todas as urls do sistema

---
+++ Sicre/src/componentes/CaminhoURL.java
@@ -0,0 +1,16 @@
+package componentes;
+
+public class CaminhoURL {
+
+	// Páginas gerais do sistema
+	public static final String REQUEST_PATH_HOME = "home.jsf";
+	public static final String REQUEST_PATH_INDEX = "index.jsf";
+	public static final String FACES_REDIRECT = "?faces-redirect=true";
+
+	// Páginas relacionadas ao funcionário
+	public static final String REQUEST_PATH_EMPLOYEE = "funcionarioEditar.jsf";
+	public static final String REQUEST_PATH_EMPLOYEE_LIST = "funcionarioListar.jsf";
+	
+	
+
+}
