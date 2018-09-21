dada599fc325dc1cf723fab2a77acfe34b0f9256
Lucas Sodré <lucassodre1996@gmail.com>
2018-09-21 17:01:18 -0300

    Alterações para a implementação do login

--- Sicre/src/modelo/dao/UsuarioDAO.java
+++ Sicre/src/modelo/dao/UsuarioDAO.java
@@ -3,14 +3,14 @@ package modelo.dao;
 import modelo.dominio.Usuario;
 
 public class UsuarioDAO {
-	
+
 	public Usuario obter(String login, String senha) {
 		Usuario novo = new Usuario();
 		novo.setLogin(login);
 		novo.setSenha(senha);
-		
+
 		return novo;
-		
+
 	}
 
 }
