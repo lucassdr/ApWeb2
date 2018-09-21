13e000e6871f14ebe055db83ff4eb80007761bdb
Lucas Sodré <lucassodre1996@gmail.com>
2018-09-21 15:18:45 -0300

    Alteração do REQUEST_PATH e da url relativos a página index.xhtml

--- Sicre/src/componentes/CaminhoURL.java
+++ Sicre/src/componentes/CaminhoURL.java
@@ -4,7 +4,7 @@ public class CaminhoURL {
 
 	// Páginas gerais do sistema
 	public static final String REQUEST_PATH_HOME = "home.jsf";
-	public static final String REQUEST_PATH_INDEX = "index.jsf";
+	public static final String REQUEST_PATH_LOGIN = "login.jsf";
 	public static final String FACES_REDIRECT = "?faces-redirect=true";
 
 	// Páginas relacionadas ao funcionário
